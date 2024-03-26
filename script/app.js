7eacd6ead8a59b3d79246e8d6c0b7ae4d0dd69c9
1standone <145244011+1standone@users.noreply.github.com>
2024-03-26 23:14:57 +0700

    Update app.js

--- script/app.js
+++ script/app.js
@@ -19,6 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	const dropdownBtn = document.querySelector('.dropdown');
 	const dropdownContent = document.querySelector('.dropdown-content');
 	const searchInput = document.querySelector('#header-search-input')
+
 	dropdownBtn.addEventListener('click', (e) => {
 		dropdownBtn.classList.toggle('open')
 		if (dropdownBtn.classList.contains('open')) {
@@ -26,7 +27,9 @@ document.addEventListener('DOMContentLoaded', () => {
 			searchInput.focus()
 			searchInput.addEventListener('blur', () => {
 				dropdownContent.style.display = 'none'
-				dropdownBtn.classList.remove('open')
+				if (dropdownBtn.classList.contains('open')) {
+					dropdownBtn.classList.remove('open')
+				}
 			})
 		}
 		else {
@@ -35,6 +38,72 @@ document.addEventListener('DOMContentLoaded', () => {
 	})
 	dropdownContent.addEventListener('click', function(e) {
 		e.stopPropagation()
+	})
+
+	const bannerForm = document.querySelector('.banner .left-side > form');
+	const bannerFormInput = document.querySelectorAll('.banner .left-side > form input')
+	const textInputContainer = document.querySelectorAll('.banner .left-side > form p');
+	let lastIndexClicked = null
+	bannerFormInput.forEach((inp, index) => {
+		inp.addEventListener('click', (e) => {
+			if (lastIndexClicked !==  null) {
+				// inp[lastIndexClicked].style.display = 'none'
+				bannerFormInput[lastIndexClicked].style.display = 'none'
+			}
+			e.target.style.display = 'block';
+			e.target.focus()
+			lastIndexClicked = index;
+		})
+		inp.addEventListener('blur', () => {
+			inp.style.display = 'none'
+		})
+		inp.addEventListener('input', () => {
+			textInputContainer.forEach((textInp) => {
+				textInputContainer[index].innerHTML = inp.value
+			})
+		})
 
 	})
+
+	const containerCard = document.querySelector('.card-container');
+	const cards = document.querySelectorAll('.card');
+
+	const arrRight = document.querySelector('.container .right');
+	const arrLeft = document.querySelector('.container .left');
+
+	let width = 330
+	let count = 3;
+	console.log(screen.width)
+	window.addEventListener('resize', () => {
+		if (screen.width < 880 && screen.width > 477) {
+			count = 2
+			console.log('true')
+		}
+		else if (screen.width < 476) {
+			count = 1
+		}
+	})
+	window.addEventListener('load', () => {
+		if (screen.width < 880 && screen.width > 477) {
+			count = 2
+			console.log('true')
+		}
+		else if (screen.width < 476) {
+			count = 1
+		}
+	})
+
+	let position = 0;
+	arrRight.addEventListener('click', () => {
+		position = position - width * count;
+		position = Math.max(position, -width * (cards.length - count));
+		containerCard.style.marginLeft = position + 'px'
+	})
+
+	arrLeft.addEventListener('click', () => {
+		position += width * count
+		position = Math.min(position, 0)
+		containerCard.style.marginLeft = position + 'px'
+	})
+
 }) 
